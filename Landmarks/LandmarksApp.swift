4878c6d44c9e5cf9c34f6a2fb36b1a9e7458c605
Rodrigo Brandão <k-noon@hotmail.com>
2021-04-11 22:06:27 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rodrigo Brandão on 11/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
